c66bc7e081e8ff64c4ad7a5f7cd152a2b45332a8
Oscar Bark <oscar.bark@bitaddict.se>
2020-07-30 14:32:47 +0200

    Make implementation more efficient by vectorization

--- main2.py
+++ main2.py
@@ -10,96 +10,99 @@ cfg_init = {
     'deposit_monthly': 10000,
     'return_annual_mean': (1+0.008027)**12-1, #0.04,
     'return_monthly_std': 0.054027,
-    'standard_rate_annual_mean': 0.003,
+    'standard_rate_annual_mean': 0.005,
     'n_months': 480,
     'annual_raise': 0.03,
     'months_of_interest': [60, 120, 240, 360],
-    'n_trials': 500
+    'n_trajectories': 500
 }
 
 with open('kde.pickle', 'rb') as file:
         monthly_return_kde = pickle.load(file)
 
-
-
-
 class Simulator:
 
     def __init__(self, cfg):
         self.cfg = cfg
+        self.allocate()
 
     def set_cfg(self, cfg):
         self.cfg = cfg
+        self.allocate()
 
     def update_cfg_item(self, key, val):
         if key not in self.cfg.keys():
             error_msg = f"{key} not in cfg"
             raise KeyError(error_msg)
         self.cfg[key] = val
+        self.allocate()
 
-    def simulate_trajectory(self):
-    #n_months, value_init, deposit_amount=0, return_annual_mean=0, standard_rate_annual_mean=0.0125, annual_raise=0):
-        value_history = []
-        deposit_history = []
-        return_history = []
+    def allocate(self):
+        self.value_trajectories = np.zeros((self.cfg['n_trajectories'], self.cfg['n_months']+1))
+        self.deposit_trajectories = np.zeros((self.cfg['n_trajectories'], self.cfg['n_months']))
+        self.return_trajectories = np.zeros((self.cfg['n_trajectories'], self.cfg['n_months']))
+        self.standard_rate_trajectories = np.zeros((self.cfg['n_trajectories'], (self.cfg['n_months']+1)//12))
+        self.value_median_trajectory = np.zeros((self.cfg['n_months']+1))
+        self.value_mean_trajectory = np.zeros((self.cfg['n_months']+1))
+        self.value_95_quant_trajectory = np.zeros((self.cfg['n_months']+1))
+        self.value_05_quant_trajectory = np.zeros((self.cfg['n_months']+1))
+
+    def simulate_trajectories(self):
 
         # Pre-calculations
-        return_monthly_mean = (1+self.cfg['return_annual_mean'])**(1/12)-1
-        return_monthly_std = self.cfg['return_monthly_std']
+        # return_monthly_mean = (1+self.cfg['return_annual_mean'])**(1/12)-1
+        # return_monthly_std = self.cfg['return_monthly_std']
 
-        value_history.append(self.cfg['value_init'])
+        self.value_trajectories[:, 0] = self.cfg['value_init']
+        self.return_trajectories = self.__sample_monthly_returns() #shape=[n_trajectories, n_months]
+        self.deposit_trajectories = self.__sample_monthly_deposits() #shape=[n_trajectories, n_months]
+        self.standard_rate_trajectories = self.__sample_annual_standard_rates() #shape=[n_trajectories, (n_months+1)/12]
         for iMonth in range(self.cfg['n_months']):
-            return_history.append(self.__sample_monthly_return(return_monthly_mean, return_monthly_std))
-            deposit_history.append(self.__sample_monthly_deposit(iMonth, self.cfg['deposit_monthly'], self.cfg['annual_raise']))
-            value_new = self.__get_new_portfolio_value(value_history, return_history, deposit_history, self.cfg['standard_rate_annual_mean'])
-            value_history.append(value_new)
+            self.value_trajectories[:, iMonth+1] = self.value_trajectories[:, iMonth] * (1 + self.return_trajectories[:, iMonth]) + self.deposit_trajectories[:, iMonth]
+            if (iMonth%12==0) and (iMonth > 0):
+                standard_rates_month = self.standard_rate_trajectories[:, iMonth//12]
+                taxes = self.__tax_isk(standard_rates_month, iMonth)
+                self.value_trajectories[:, iMonth+1] = self.value_trajectories[:, iMonth+1] - taxes
 
-        return value_history, return_history, deposit_history, self.cfg['value_init']
+        return self.value_trajectories, self.return_trajectories, self.deposit_trajectories
 
-    def simulate_trajectories(self, n_trajectories):
-        vals = np.zeros((n_trajectories, self.cfg['n_months']+1))
-        for i in range(self.cfg['n_trials']):
-                val, _, dep, _ = self.simulate_trajectory()
-                vals[i, :] = val
-        val_mean = list(np.mean(vals, axis=0))
-        val_95 = list(np.quantile(vals, 0.95, axis=0))
-        val_05 = list(np.quantile(vals, 0.05, axis=0))
-        return val_mean, val_05, val_95, dep, self.cfg['value_init']
+    def calculate_statistical_trajectories(self):
+        self.value_mean_trajectory = np.mean(self.value_trajectories, axis=0)
+        self.value_median_trajectory, self.value_05_quant_trajectory, self.value_95_quant_trajectory = np.quantile(self.value_trajectories, [0.5, 0.05, 0.95], axis=0)
+        return self.value_mean_trajectory, self.value_median_trajectory, self.value_05_quant_trajectory, self.value_95_quant_trajectory
 
-    # Calculate the new portfolio value as we enter a new month, expand histories
-    def __get_new_portfolio_value(self, value_history, return_history, deposit_history, standard_rate_annual_mean):
-        value_curr = value_history[-1] # v_i, value of portfolio at start of month i, before deposit
-        return_curr = return_history[-1] # r_i, total return at the end of month i
-        deposit_curr = deposit_history[-1] # d_i, deposited amount at the start of month i
-        #assert len(value_history) == (len(return_history) + 1), f"{len(value_history)}, {len(return_history)}"
-        #assert len(value_history) == (len(deposit_history) + 1)
+    def get_shit(self):
+        #self.allocate()
+        self.simulate_trajectories()
+        self.calculate_statistical_trajectories()    
+        return self.value_mean_trajectory, self.value_05_quant_trajectory, self.value_95_quant_trajectory, self.deposit_trajectories[0,:], self.cfg['value_init']
 
-        value_new = value_curr * (1 + return_curr)
-        value_new = value_new + deposit_curr
+    def __sample_monthly_returns(self):
+        n = self.cfg['n_trajectories']*self.cfg['n_months'] 
+        return monthly_return_kde.resample(n).reshape(self.cfg['n_trajectories'], self.cfg['n_months'])
 
-        month_idx = len(value_history)-1
-        if (month_idx%12 == 0) and (month_idx > 0):
-            standard_rate = self.__sample_standard_rate_annual(standard_rate_annual_mean)
-            value_new = value_new - self.__tax_isk(standard_rate, value_history, deposit_history, month_idx)
+    def __sample_monthly_deposits(self):
+        monthly_deposit_trajectory = self.cfg['deposit_monthly']*(1+self.cfg['annual_raise'])**(np.arange(self.cfg['n_months']) // 12)
+        return np.repeat(monthly_deposit_trajectory[np.newaxis, :], self.cfg['n_trajectories'], axis=0)
 
-        return value_new
+    def __sample_annual_standard_rates(self):
+        return self.cfg['standard_rate_annual_mean']*np.ones((self.cfg['n_trajectories'], (self.cfg['n_months']+1)//12))
 
-    def __sample_monthly_return(self, return_monthly_mean, return_monthly_std=0):
-        return monthly_return_kde.resample(1)[0,0]
-        if return_monthly_std == 0:
-            return return_monthly_mean
-        else:
-            return np.random.normal(return_monthly_mean, return_monthly_std)
+    def __tax_isk(self, standard_rates_month, month_idx):
+        """ 
+            Parameters
+            --------------
+            standard_rate : np.ndarray, shape=[n_trajectories]
+        """
+        tax_rate = np.maximum(0.0125, 0.01+standard_rates_month)
+        return tax_rate*( \
+            self.value_trajectories[:, month_idx-12]+ \
+            self.value_trajectories[:, month_idx-9]+ \
+            self.value_trajectories[:, month_idx-6]+ \
+            self.value_trajectories[:, month_idx-3]+ \
+            self.deposit_trajectories[:, month_idx-12:month_idx].sum(axis=1) \
+            ) / 4
 
-    def __sample_monthly_deposit(self, month_idx, deposit_amount, annual_raise):
-        return deposit_amount*(1+annual_raise)**(month_idx // 12)
-
-    def __sample_standard_rate_annual(self, standard_rate_annual_mean):
-        return standard_rate_annual_mean
-
-    def __tax_isk(self, standard_rate, value_history, deposit_history, month_idx):
-        tax_rate = max(0.0125, 0.01+standard_rate)
-        return tax_rate*(value_history[month_idx-12]+value_history[month_idx-9]+value_history[month_idx-6]+value_history[month_idx-3] + sum(deposit_history[month_idx-12:month_idx])) / 4
 
 
 class MainWindow:
@@ -108,10 +111,9 @@ class MainWindow:
 
     def __init__(self, cfg):
         self.simulator = Simulator(cfg)
-        self.n_trials = cfg['n_trials']
         self.__initialize_figure(cfg['n_months'])
-        val, ret, dep, val_init = self.simulator.simulate_trajectory()
-        self.__plot_trajectory(val, val, val, dep, val_init)
+        val, val_low, val_high, dep, val_init = self.simulator.get_shit()
+        self.__plot_trajectory(val, val_low, val_high, dep, val_init)
         plt.show()
 
     def __add_slider(self, key, vallims=None, valinit=None, valsteporder=None, title=None, **kwargs):
@@ -171,7 +173,7 @@ class MainWindow:
 
     def __recalculate_and_draw(self, key, val):
         self.simulator.update_cfg_item(key, val)
-        val_mean, val_05, val_95, dep, val_init = self.simulator.simulate_trajectories(self.n_trials)
+        val_mean, val_05, val_95, dep, val_init = self.simulator.get_shit()
         self.__plot_trajectory(val_mean, val_05, val_95, dep, val_init)
 
     def __plot_trajectory(self, val, val_low, val_high, dep, value_init):
@@ -198,7 +200,6 @@ class MainWindow:
         # Update scale if necessary
         lims_y = self.ax_plot.get_ylim()
         if (max(val_high) > lims_y[1]*0.9) or (max(val_high) < lims_y[1] / 10) or (min(val_low) < lims_y[0]):
-            print("Autoscaling")
             autoscale(self.ax_plot, axis='y', factor=2)
 
 
