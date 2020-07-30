8f9f6e50e2dad8a82e35c5c85841ae19c0527610
Oscar Bark <oscar.bark@bitaddict.se>
2020-07-30 09:34:52 +0200

    Added simulating multiple trajectories

--- main2.py
+++ main2.py
@@ -12,7 +12,8 @@ cfg_init = {
     'standard_rate_annual_mean': 0.003,
     'n_months': 480,
     'annual_raise': 0.03,
-    'months_of_interest': [60, 120, 240, 360]
+    'months_of_interest': [60, 120, 240, 360],
+    'n_trials': 500
 }
 
 def sample_monthly_return(return_monthly_mean, return_monthly_std=0):
@@ -78,7 +79,7 @@ class MainWindow:
         self.cfg = cfg
         self.__initialize_figure(self.cfg)
         val, ret, dep = simulate_trajectory(self.cfg)
-        self.__plot_trajectory(val)
+        self.__plot_trajectory(val, val, val, dep)
         plt.show()
 
     def __add_slider(self, key, vallims=None, valinit=None, valsteporder=None, title=None, **kwargs):
@@ -131,7 +132,11 @@ class MainWindow:
         self.ax_plot.grid(axis='y', which='both')
         self.ax_plot.set_xlabel("Years from now")
         self.ax_plot.set_ylabel("Capital (kr)")
-        self.trajectory_line = None
+        self.ax_plot.set_xlim([0, (self.cfg['n_months']+1)/12])
+        self.trajectory_line = None        
+        self.trajectory_line_low = None        
+        self.trajectory_line_high = None
+        self.deposit_line = None
 
         self.sliders = {}
         self.slider_counter = 0
@@ -160,18 +165,40 @@ class MainWindow:
 
     def __recalculate_and_draw(self, key, val):
         self.__update_cfg(key, val)
-        val, ret, dep = simulate_trajectory(self.cfg)
-        self.__plot_trajectory(val)
+        vals = np.zeros((self.cfg['n_trials'], self.cfg['n_months']+1))
+        for i in range(self.cfg['n_trials']):
+            val, _, dep = simulate_trajectory(self.cfg)
+            vals[i, :] = val
+        val_mean = list(np.mean(vals, axis=0))
+        val_95 = list(np.quantile(vals, 0.95, axis=0))
+        val_05 = list(np.quantile(vals, 0.05, axis=0))
+        print(val_05)
+        self.__plot_trajectory(val_mean, val_05, val_95, dep)
 
-    def __plot_trajectory(self, val):
+    def __plot_trajectory(self, val, val_low, val_high, dep):
         if self.trajectory_line is not None:
             self.trajectory_line.set_ydata(val)
         else:
             self.trajectory_line = self.ax_plot.semilogy([i/12 for i in range(len(val))], val)[0]
 
+        if self.trajectory_line_low is not None:
+            self.trajectory_line_low.set_ydata(val_low)
+        else:
+            self.trajectory_line_low = self.ax_plot.semilogy([i/12 for i in range(len(val_low))], val_low)[0]
+
+        if self.trajectory_line_high is not None:
+            self.trajectory_line_high.set_ydata(val_high)
+        else:
+            self.trajectory_line_high = self.ax_plot.semilogy([i/12 for i in range(len(val_high))], val_high)[0]
+
+        if self.deposit_line is not None:
+            self.deposit_line.set_ydata(self.cfg['value_init']+np.cumsum(np.array(dep)))
+        else:
+            self.deposit_line = self.ax_plot.semilogy([i/12 for i in range(len(dep))], self.cfg['value_init']+np.cumsum(np.array(dep)))[0]
+
         # Update scale if necessary
         lims_y = self.ax_plot.get_ylim()
-        if (max(val) > lims_y[1]*0.9) or (max(val) < lims_y[1] / 10):
+        if (max(val_high) > lims_y[1]*0.9) or (max(val_high) < lims_y[1] / 10) or (min(val_low) < lims_y[0]):
             print("Autoscaling")
             autoscale(self.ax_plot, axis='y', factor=2)
 
